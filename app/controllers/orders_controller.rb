1b611dc8d391e1644b077d27b9eb75361123cd9b
LindsAndrus <LindsAndrus@hotmail.com>
2016-04-27 11:01:20 -0700

    other update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,15 @@ class OrdersController < ApplicationController
   end
 
   def update
+    for idx in session[:cartItems]
+      if params[:id] == idx
+        flash[:message] = "This item is already in your cart"
+        redirect_to '/'
+      else
+        (session[:cartItems] ||= []) << params[:id]
+        redirect_to '/' and return
+      end
+    end
   end
 
   def destroy
